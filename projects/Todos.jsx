e0f5bc829aae4ef6706fd2aeab62599cc1b51286
darkgogi <sukkkang19@naver.com>
2019-01-29 17:58:31 +0900

    unmount mount test 예제

--- projects/Todos.jsx
+++ projects/Todos.jsx
@@ -2,19 +2,30 @@ import React from 'react';
 import './Todos.css'
 
 
-const Todos = (props) => {
+class Todos extends React.Component{
 
-  return (
-    <div className="Todos" style={ {backgroundColor:'#ccc'}}>
-      <h2>{props.title}</h2>
-      <ul>
-        {props.items.map((item, index) => {
-          return <li key={index}><input type="checkbox" checked={item.completed}/> {item.name} </li>
-        })}
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
 
-      </ul>
-    </div>
-  )
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+
+  render() {
+    const {title, items} = this.props;
+
+    return (
+      <div className="Todos" style={ {backgroundColor:'#ccc'}}>
+        <h2>{title}</h2>
+        <ul>
+          {items.map((item, index) => {
+            return <li key={index}><input type="checkbox" checked={item.completed}/> {item.name} </li>
+          })}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default Todos;
