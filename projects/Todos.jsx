0e4f769870671f0673bb41033a04dc6395258d17
darkgogi <sukkkang19@naver.com>
2019-01-29 17:19:59 +0900

    second

--- projects/Todos.jsx
+++ projects/Todos.jsx
@@ -4,25 +4,10 @@ import './Todos.css'
 
 const Todos = (props) => {
 
-  console.log('----->', props);
   return (
     <div className="Todos" style={ {backgroundColor:'#ccc'}}>
       <h2>{props.title}</h2>
       <ul>
-
-        {/*<li><input type="checkbox"/> {props.items[0]} </li>*/}
-        {/*<li><input type="checkbox"/> {props.items[1]} </li>*/}
-        {/*<li><input type="checkbox"/> {props.items[2]} </li>*/}
-        {/*<li><input type="checkbox"/> {props.items[3]} </li>*/}
-        {/*<li><input type="checkbox"/> {props.items[4]} </li>*/}
-        {/*<li><input type="checkbox"/> {props.items[5]} </li>*/}
-
-        {/*{props.items.map((item, index) => {*/}
-          {/*// console.log('====>>', item);*/}
-          {/*return <li key={index}><input type="checkbox"/> {item} </li>*/}
-        {/*})}*/}
-
-
         {props.items.map((item, index) => {
           return <li key={index}><input type="checkbox" checked={item.completed}/> {item.name} </li>
         })}
